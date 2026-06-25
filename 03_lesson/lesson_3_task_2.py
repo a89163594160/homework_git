f9f41bfcb871685d53c9f66386e17730b325ffaf
a89163594160 <a89163594160@gmail.com>
2026-06-25 20:15:35 +0300

    Исправила файл третьего урока

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -1,6 +1,6 @@
 from smartphone import Smartphone
 
-cataloge = [
+catalog = [
     Smartphone("Samsung", "Galaxy S26", '+79163456789'),
     Smartphone("Samsung", "Galaxy S25 Ultra", '+79207896760'),
     Smartphone("Samsung", "Galaxy S25 Plus", '+79263456735'),
@@ -8,5 +8,5 @@ cataloge = [
     Smartphone("Samsung", "Galaxy S25 Edge", '+79163768309')
     ]
 
-for smartphone in cataloge:
+for smartphone in catalog:
     print(f'{smartphone.brand}, {smartphone.model}, {smartphone.number}')
